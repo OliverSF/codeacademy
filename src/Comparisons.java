da2c5fa982ffa5f1bdda315adf8f3fcc2bc48c73
OliverSF <o.faherty1@nuigalway.ie>
2017-06-10 15:11:25 +0100

    First commit

---
+++ src/Comparisons.java
@@ -0,0 +1,13 @@
+
+public class Comparisons {
+
+	public static void main(String[] args) {
+	if ( 3 >= 3 && !(true || true) ) {
+		System.out.println("Yay");
+	}
+	else { System.out.println("OH NO");
+	
+	}
+
+}
+}
